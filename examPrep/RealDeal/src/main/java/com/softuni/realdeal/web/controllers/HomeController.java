e9a8c62f67171a16e1e9af4b09d419f2a46f7283
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-02-08 15:34:45 +0200

    Added import cars xml logic

--- examPrep/RealDeal/src/main/java/com/softuni/realdeal/web/controllers/HomeController.java
+++ examPrep/RealDeal/src/main/java/com/softuni/realdeal/web/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.softuni.realdeal.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,4 +13,12 @@ public class HomeController extends BaseController{
     public ModelAndView index(){
         return this.view("index");
     }
+    @GetMapping("/home")
+    public ModelAndView home(){
+        ModelAndView modelAndView = super.view("home");
+        boolean areImported = false;
+        modelAndView.addObject("areImported",false);
+        return modelAndView;
+    }
+
 }
--- examPrep/RealDeal/src/main/java/com/softuni/realdeal/web/controllers/ImportController.java
+++ examPrep/RealDeal/src/main/java/com/softuni/realdeal/web/controllers/ImportController.java
@@ -7,8 +7,11 @@ import com.softuni.realdeal.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/import")
@@ -36,4 +39,26 @@ public class ImportController extends BaseController{
 //        };
 //        return super.view("json/import-json","areImported",areImported);
 //    }
+
+    @GetMapping("/xml")
+    public ModelAndView xml(){
+        ModelAndView modelAndView = super.view("xml/import-xml");
+        boolean[] areImported=new boolean[]{this.carService.areImported(),this.offerService.areImported(),
+                this.pictureService.areImported(),this.sellerService.areImported()};
+        modelAndView.addObject("areImported",areImported);
+        return modelAndView;
+    }
+
+    @GetMapping("/cars")
+    public ModelAndView cars() throws IOException {
+        ModelAndView modelAndView = super.view("xml/import-cars");
+        modelAndView.addObject("cars",this.carService.readCarsFileContent());
+        return modelAndView;
+    }
+
+    @PostMapping("/cars")
+    public  ModelAndView carsConfirm() throws IOException {
+        this.carService.importCars();
+        return super.redirect("/import/xml");
+    }
 }
